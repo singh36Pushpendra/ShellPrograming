65e9aa84780478cf8aafd8efcb79aa9e4a54e659
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-04 10:36:53 +0530

    modified in UsingSwitchCaseUC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,6 +5,7 @@ ABSENT=0
 PRESENT=1
 
 dailyWage=0
+monthlyWage=0
 WAGE_PER_HOUR=20
 fullDayHour=0
 
@@ -12,6 +13,7 @@ PART_TIME_HOURS=4
 FULL_TIME_HOURS=8
 isPartOrFull=$((RANDOM%2))
 
+WORKING_DAY_PER_MONTH=20
 case $attendance in 
     $PRESENT)
         echo "Employee is present!"
@@ -33,3 +35,6 @@ esac
 
 dailyWage=$((WAGE_PER_HOUR*fullDayHour))
 echo "Daily wage of employee : $dailyWage"
+
+monthlyWage=$((WORKING_DAY_PER_MONTH*WAGE_PER_HOUR*fullDayHour))
+echo "Daily wage of employee : $monthlyWage"
