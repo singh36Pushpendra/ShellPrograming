0903e7c82a57af0fc6b27db1941bf854cc422ca9
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-04 08:38:11 +0530

    modified in EmployeeDailyWageUC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,9 +4,17 @@ attendance=$((RANDOM%2))
 ABSENT=0
 PRESENT=1
 
+dailyWage=0
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=0
+
 if [ $attendance -eq $PRESENT ]
 then
     echo "Employee is present!"
+    FULL_DAY_HOUR=8
 else
     echo "Employee is absent!"
 fi
+
+dailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
+echo "Daily wage of employee : $dailyWage"
