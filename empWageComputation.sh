6c9f722f83b38880d3f17e85ec0deed892be6c5a
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-04 09:20:11 +0530

    modified in AddParttimeUC

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,20 +1,33 @@
-#!/bin/bash
-echo "Welcome to Employee Wage Computation Program on Master(piku) branch!"
-attendance=$((RANDOM%2))
+#!/bin/bash 
+echo "Welcome to Employee Wage Computation Program on Master(piku) branch!" 
+attendance=$((RANDOM%2)) 
 ABSENT=0
 PRESENT=1
 
 dailyWage=0
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=0
+fullDayHour=0
+
+PART_TIME_HOURS=4
+FULL_TIME_HOURS=8
+isPartOrFull=$((RANDOM%2))
 
 if [ $attendance -eq $PRESENT ]
 then
     echo "Employee is present!"
-    FULL_DAY_HOUR=8
+    case $isPartOrFull in
+	1)
+            fullDayHour=$FULL_TIME_HOURS
+	    echo "Full Time Employee!"
+	    ;;
+	0)
+            fullDayHour=$PART_TIME_HOURS
+	    echo "Part Time Employee!"
+	    ;;
+    esac
 else
     echo "Employee is absent!"
 fi
 
-dailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
+dailyWage=$((WAGE_PER_HOUR*fullDayHour))
 echo "Daily wage of employee : $dailyWage"
