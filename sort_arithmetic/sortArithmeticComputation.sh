ea4e20a799ff29825bdb73bdbf1ef6119c09600b
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-07 23:00:50 +0530

    modified in ComputedResultDictSA

--- sort_arithmetic/sortArithmeticComputation.sh
+++ sort_arithmetic/sortArithmeticComputation.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 
+declare -A dictArithmetics
 read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c
-echo $((a+b*c))
-echo $((a*b+c))
-echo $((c+a/b))
-echo $((a%b+c))
+dictArithmetics[1]=$((a+b*c))
+dictArithmetics[2]=$((a*b+c))
+dictArithmetics[3]=$((c+a/b))
+dictArithmetics[4]=$((a%b+c))
+echo ${dictArithmetics[@]}
